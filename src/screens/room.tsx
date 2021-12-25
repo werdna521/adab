9f0c8c812a3d7be411849930251d759130def762
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2021-12-25 17:11:31 +0700

    feat: add google engine speech to text (#1)
    * feat: build custom eas dev client
    
    * feat: implement stt
    
    Co-authored-by: andrewmt0 <andrewmt0@users.noreply.github.com>

--- src/screens/room.tsx
+++ src/screens/room.tsx
@@ -1,14 +1,14 @@
+import Voice from '@react-native-voice/voice'
+import { SpeechResultsEvent } from '@react-native-voice/voice/src/VoiceModuleTypes'
 import { RouteProp, useRoute } from '@react-navigation/native'
 import React, { FC, useEffect, useState } from 'react'
-import { Text, View, StyleSheet, TextInput, Button } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 import { RootStackParamList } from '../components/navigator/root'
-import { streamContent } from '../lib/firebase/db/stream-content'
-import { updateContent } from '../lib/firebase/db/update-content'
+import { streamContent, updateContent } from '../lib/firebase/db'
 
 type CreateRoomScreenRouteProp = RouteProp<RootStackParamList, 'Room'>
 
 const RoomScreen: FC = () => {
-  const [value, setValue] = useState('')
   const [content, setContent] = useState('')
   const route = useRoute<CreateRoomScreenRouteProp>()
   const { roomId } = route.params
@@ -16,36 +16,66 @@ const RoomScreen: FC = () => {
   useEffect(() => {
     const handleStream = (stream: string) => setContent(stream)
 
-    const unSubscribe = streamContent({
+    // stream room content from firestore
+    const unsubscribe = streamContent({
       roomId,
       onStream: handleStream,
     })
 
     return () => {
-      unSubscribe()
+      // unsubscribe from firestore when leaving screen
+      unsubscribe()
     }
   }, [])
 
-  const handleSend = async () => {
-    const { error } = await updateContent({
-      roomId,
-      content: `${content} ${value}`,
-    })
-    if (error) return alert('cannot send message for some reason')
+  useEffect(() => {
+    let speechResult = ''
+    let contentText = content
 
-    setValue('')
-  }
+    Voice.onSpeechStart = () => console.log('start')
+    Voice.onSpeechPartialResults = (event: SpeechResultsEvent) => {
+      const text = event?.value?.[0]
+      if (text) {
+        speechResult = text
+      }
+    }
+    Voice.onSpeechEnd = async () => {
+      console.log('end')
+
+      if (speechResult) {
+        const { error } = await updateContent({
+          roomId,
+          content: `${contentText} ${speechResult}`,
+        })
+        if (error) return alert('cannot send message for some reason')
+
+        contentText = `${contentText} ${speechResult}`
+        speechResult = ''
+      }
+
+      Voice.start('id', {
+        EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS: 0,
+      })
+    }
+    Voice.onSpeechError = () => {
+      console.log('error')
+      Voice.start('id', {
+        EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS: 0,
+      })
+    }
+    Voice.onSpeechRecognized = () => console.log('recognized')
+
+    Voice.start('id', {
+      EXTRA_SPEECH_INPUT_MINIMUM_LENGTH_MILLIS: 0,
+    })
+    return () => {
+      Voice.destroy()
+    }
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Text>Room</Text>
-      <TextInput
-        style={styles.textInput}
-        placeholder="Chat Test"
-        value={value}
-        onChangeText={setValue}
-      />
-      <Button title="Send Text" onPress={handleSend} />
+      <Text>Room </Text>
       <Text>{content}</Text>
     </View>
   )
